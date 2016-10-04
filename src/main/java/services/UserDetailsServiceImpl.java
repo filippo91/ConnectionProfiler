b4371fd3cc213024f6fbd9de97957d9c9abd39ca
filippo91 <heartbreakid91@gmail.com>
2016-10-04 18:02:43 +0200

    fix: Wrong import removed

--- src/main/java/services/UserDetailsServiceImpl.java
+++ src/main/java/services/UserDetailsServiceImpl.java
@@ -13,8 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import models.Authority;
 import models.User;
 import repositories.UserRepository;
 

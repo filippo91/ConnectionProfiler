f38d35d517d3f37a9ec39ed8798a83997f05e4b5
filippo91 <heartbreakid91@gmail.com>
2016-10-10 23:40:26 +0200

    fix: use Long to be coherent with Download object

--- src/main/java/models/SizeDownload.java
+++ src/main/java/models/SizeDownload.java
@@ -1,13 +1,13 @@
 package models;
 
 public class SizeDownload {
-	private Integer size;
+	private Long size;
 	private String server_domain;
 	
-	public Integer getSize() {
+	public Long getSize() {
 		return size;
 	}
-	public void setSize(Integer size) {
+	public void setSize(Long size) {
 		this.size = size;
 	}
 	public String getServer_domain() {
@@ -16,6 +16,37 @@ public class SizeDownload {
 	public void setServer_domain(String server_domain) {
 		this.server_domain = server_domain;
 	}
-	
-	
+	@Override
+	public String toString() {
+		return "SizeDownload [size=" + size + ", server_domain=" + server_domain + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((server_domain == null) ? 0 : server_domain.hashCode());
+		result = prime * result + ((size == null) ? 0 : size.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof SizeDownload))
+			return false;
+		SizeDownload other = (SizeDownload) obj;
+		if (server_domain == null) {
+			if (other.server_domain != null)
+				return false;
+		} else if (!server_domain.equals(other.server_domain))
+			return false;
+		if (size == null) {
+			if (other.size != null)
+				return false;
+		} else if (!size.equals(other.size))
+			return false;
+		return true;
+	}
 }
